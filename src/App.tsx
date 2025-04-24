11a4af2bbbfb347b0e67826173771369cc0d0a0e
njihiamark <njihiamark@gmail.com>
2025-04-24 18:11:20 +0300

    core routes

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-function App() {
-  return (
-    <h1 className="text-3xl font-bold underline text-center">Vite + React</h1>
-  );
-}
-
-export default App;
---
+++ src/routes/_unauthenticated/route.tsx
@@ -0,0 +1,13 @@
+import { createFileRoute, Outlet } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/_unauthenticated")({
+  component: RouteComponent,
+});
+
+function RouteComponent() {
+  return (
+    <div>
+      <Outlet />
+    </div>
+  );
+}
