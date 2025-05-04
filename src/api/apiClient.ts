d79bdfc34dbe07c278cb7249cc10fff109af3dde
njihiamark <njihiamark@gmail.com>
2025-05-04 21:46:50 +0300

    token refreshing

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { useAuthStore } from "../zustand-stores/auth";
 
 const apiClient = axios.create({
   baseURL: import.meta.env.VITE_SERVER_URL,
@@ -8,5 +9,53 @@ const apiClient = axios.create({
     "Content-Type": "application/json",
   },
 });
+
+let refreshPromise: Promise<void> | null = null;
+let isRefreshing = false;
+
+// Token refresh function
+const refreshToken = async () => {
+  if (!refreshPromise) {
+    isRefreshing = true;
+    refreshPromise = apiClient
+      .post("/auth/refresh")
+      .then(() => {})
+      .finally(() => {
+        isRefreshing = false;
+        refreshPromise = null;
+      });
+  }
+  await refreshPromise;
+};
+
+// Response interceptor
+apiClient.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response?.status === 401) {
+      if (!originalRequest._retry) {
+        originalRequest._retry = true;
+        try {
+          await refreshToken();
+          return apiClient(originalRequest);
+        } catch (refreshError) {
+          useAuthStore.setState({ isAuthenticated: false, currentUser: null });
+
+          // Prevent redirect if already on login or signup pages
+          const publicPaths = ["/", "/sign-up"];
+          if (!publicPaths.includes(window.location.pathname)) {
+            window.location.href = "/";
+          }
+
+          return Promise.reject(refreshError);
+        }
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
 
 export default apiClient;
